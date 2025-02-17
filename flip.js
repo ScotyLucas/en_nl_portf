fc97617e47d92830cd08993357d926e9ec374801
ScotyLucas <155391337+ScotyLucas@users.noreply.github.com>
2025-02-17 20:01:50 +0100

    Create flip.js

---
+++ flip.js
@@ -0,0 +1,4 @@
+// Kattintás esemény a doboz flip hatásának indításához
+document.querySelector('.click-text').addEventListener('click', function() {
+    document.querySelector('.box').classList.toggle('flip');
+});
